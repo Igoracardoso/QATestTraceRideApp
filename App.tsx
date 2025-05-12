f1efae1fc3f7ce2e985edd86fa4514ceef0b69a1
Igoracardoso <igorcardosoofcial@gmail.com>
2025-05-12 19:52:30 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParamList } from './types';
+import LoginScreen from './src/screens/LoginScreean';
+import VehiclesScreen from './src/screens/VehiclesScreen';
+import VehicleMapScreen from './src/screens/VehicleMapScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import VehicleRegistrationScreen from './src/screens/VehicleRegistrationScreen';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Vehicles"
+          component={VehiclesScreen}
+          options={{ title: 'My Vehicles' }}
+        />
+        <Stack.Screen
+          name="VehicleMap"
+          component={VehicleMapScreen}
+          options={{ title: 'Vehicle Location' }}
+        />
+          <Stack.Screen
+          name="VehicleRegister"
+          component={VehicleRegistrationScreen}
+          options={{ title: 'Vehicle Register' }}
+        />
+        <Stack.Screen
+        name="Register"
+        component={RegisterScreen}
+        options={{ headerShown: false }}
+        
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
